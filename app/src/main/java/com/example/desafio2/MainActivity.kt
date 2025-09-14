f3853be7d0f8545c629ccf4adf96b962ffe71a49
milennne <milene.fuentes@tecsup.edu.pe>
2025-09-13 23:26:01 -0500

    COMMIT DE MILENE

--- app/src/main/java/com/example/desafio2/MainActivity.kt
+++ app/src/main/java/com/example/desafio2/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,17 +23,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DESAFIO2Theme {
-                TaskCompletedScreen()
+                TareaCompleta()
             }
         }
     }
 }
 
 @Composable
-fun TaskCompletedScreen() {
+fun TareaCompleta() {
     Column(
-        modifier = Modifier
-            .fillMaxSize(),
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -42,15 +40,14 @@ fun TaskCompletedScreen() {
             painter = painterResource(id = R.drawable.ic_task_completed),
             contentDescription = null
         )
-
         Text(
-            text = stringResource(R.string.all_tasks_completed),
+            text = "All tasks completed",
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
         )
 
         Text(
-            text = stringResource(R.string.nice_work),
+            text = "Nice work!",
             fontSize = 16.sp
         )
     }
@@ -60,6 +57,6 @@ fun TaskCompletedScreen() {
 @Composable
 fun TaskCompletedPreview() {
     DESAFIO2Theme {
-        TaskCompletedScreen()
+        TareaCompleta()
     }
 }
