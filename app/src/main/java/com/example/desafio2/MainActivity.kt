0778dabc0ccab38a70ec8f7573975e02b62ba350
milennne <milene.fuentes@tecsup.edu.pe>
2025-09-13 22:49:03 -0500

    COMMIT DE MILENE

---
+++ app/src/main/java/com/example/desafio2/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.desafio2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.desafio2.ui.theme.DESAFIO2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DESAFIO2Theme {
+                TaskCompletedScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskCompletedScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_task_completed),
+            contentDescription = null
+        )
+
+        Text(
+            text = stringResource(R.string.all_tasks_completed),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+
+        Text(
+            text = stringResource(R.string.nice_work),
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TaskCompletedPreview() {
+    DESAFIO2Theme {
+        TaskCompletedScreen()
+    }
+}
